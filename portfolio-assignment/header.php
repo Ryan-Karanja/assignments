59a5006aa3471af97c19048ac95211c59f17b45b
Jeff <jeff@jeff.how>
2018-04-02 13:22:20 -0400

    Update header.php

--- portfolio-assignment/header.php
+++ portfolio-assignment/header.php
@@ -12,37 +12,37 @@
  * +----------------------------------------------------------------------+
  * get_template_part()
  * +----------------------------------------------------------------------+
- * wp_title()
+ * language_attributes()
  * +----------------------------------------------------------------------+
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php _________________________ ; // Print the language attributes for the website ?>>
   <head>
     <!-- Required meta tags -->
-      <meta charset="<?php _______________________ ?>">
+      <meta charset="<?php _______________________ ; // The Charset ?>">
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- My linked styles -->
-    <?php _______________ ?>
+    <?php _______________ ; // Links from functions.php are included here ?>
 
-    <title><?php _________________________ ?></title>
+    <title><?php _________________________ ; // Print the title-tag ?></title>
   </head>
 
   <body>
     <div class="nav-wrap">
       <nav class="container">
-        <?php _______________________________________________________ ?>
+        <?php _______________________________________________________ ; // include the primary-nav.php file ?>
       </nav>
     </div>
     <header class="container">
       <h1>
-        <a href="<?php _____________________________ ?>">
-          <?php ____________________________ ?>
+        <a href="<?php _____________________________ ; // Print the home page URL ?>">
+          <?php ____________________________ ; // Print the name of your website ?>
         </a>
       </h1>
       <small class="tagline">
-        <?php _____________________________ ?>
+        <?php _____________________________ ; // Print the description of the website ?>
       </small>
     </header>
